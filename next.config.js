926b5a812df33e763facbb253c7a48d127060549
Mark <mark@bridgemarkinc.com>
2025-10-25 22:26:53 -0400

    Deploy: force rebuild

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co", // or your exact project host
+      },
+    ],
+  },
+};
+
 module.exports = nextConfig;
